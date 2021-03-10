c602c6b315cdc97ae8924dc87f74bfcc79e45686
Alex Setia <alextheprogrammer21@gmail.com>
2021-03-10 18:14:08 -0500

    Add navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import Avatar from "./Avatar";
+import "./navbar.css";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    color: "#6495ED",
+  },
+  navbar: {
+    backgroundColor: "#fff",
+    color: "black",
+    boxShadow: "0 4px 4px rgba(0, 0, 0, 0.1)",
+  },
+  links: {
+    color: "black",
+    ":hover": {
+      color: "#white",
+    },
+  },
+}));
+
+export default function ButtonAppBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" className={classes.navbar}>
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            Travel Booking
+          </Typography>
+          <Button id="links">item1</Button>
+          <Button id="links">item2</Button>
+          <Button id="links">item3</Button>
+          <Button id="links">item4</Button>
+          <Avatar />
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
